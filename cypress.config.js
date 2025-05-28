d13a7d615a300dfa6cfc032ecb6e7ded880b88df
abdulsalam-aldabik <r0918747@student.thomasmore.be>
2025-05-28 17:40:12 +0200

    fix cypress cloud

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "9aqqrn",
   e2e: {
-    projectId: "6gq7f8",
+
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
